e4d657460b6752c457fe825e7649e14803f54dd4
Amanda <mandacosta94@gmail.com>
2025-08-16 17:43:11 -0300

    Criando cluster EKS

--- vars.tf
+++ vars.tf
@@ -14,5 +14,22 @@ variable "tags" {
   default = {
     Name = "golucnh-terraform-amanda"
   }
+}
 
+variable "principal_arn" {
+  type        = string
+  description = "ARN do IAM Role que terá acesso ao cluster EKS"
+  default     = "arn:aws:iam::509689628129:role/voclabs"
+}
+
+variable "policy_arn" {
+  default = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+}
+
+variable "labRole" {
+  default = "arn:aws:iam::509689628129:role/LabRole"
+}
+
+variable "accessConfig" {
+  default = "API_AND_CONFIG_MAP"
 }
